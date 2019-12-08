ceea2f705d36533bd5063e39702b02518360ca44
Sam Stites <stites@users.noreply.github.com>
2019-12-08 00:10:03 -0500

    get parser working

--- src/Main.purs
+++ src/Main.purs
@@ -1,18 +1,74 @@
 module Main where
 
 import Prelude
+import Control.Alt
+import Data.Tuple (Tuple(Tuple))
 import Effect (Effect)
 import Effect.Console (log)
-import Data.List (filter, range, List)
-import Data.Foldable (sum)
+
+import Web.DOM.Document (Document, getElementsByTagName)
+import Web.DOM.HTMLCollection (HTMLCollection)
+import Web.HTML (window)
+import Web.HTML.Window (location)
+import Web.HTML.Location (hostname)
+
+import Data.Array as Array
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Text.Parsing.Parser
+import Text.Parsing.Parser.Combinators
+import Text.Parsing.Parser.String
+import Text.Parsing.Parser.Token
 
 main :: Effect Unit
 main = do
   log "Hello sailor!"
-  log $ show $ euler1
+  currentDomain >>= log
 
-euler1 :: Int
-euler1 = sum multiples
+currentDomain :: Effect String
+currentDomain = window >>= (location >=> hostname)
+
+inArxiv :: String -> Boolean
+inArxiv s = not $ s == "arxiv.org"
+
+getAnchors :: Document -> Effect HTMLCollection
+getAnchors = getElementsByTagName "a"
+
+data PageType = PDF | Abstract
+newtype ArxivId = ArxivId String
+
+arxivParser :: Parser String (Tuple PageType ArxivId)
+arxivParser = protocol *> domain *> page
   where
-    multiples :: List Int
-    multiples = filter (\n -> mod n 3 == 0 || mod n 5 == 0) (range 0 999)
+    protocol :: Parser String Unit
+    protocol = do
+      void (string "http")
+      optional (char 's')
+      void (string "://")
+
+    domain :: Parser String Unit
+    domain = do
+      optional (string "www.")
+      void (string "arxiv.org/")
+
+    page :: Parser String (Tuple PageType ArxivId)
+    page = do
+      stype <- try (string "pdf") <|> string "abs"
+      void (char '/')
+      l <- fromCharArray <$> Array.many digit
+      void (char '.')
+      r <- fromCharArray <$> Array.many digit
+      pure $ Tuple (if stype == "pdf" then PDF else Abstract) (ArxivId $ l <> "." <> r)
+
+-- let current_domain = window.location.hostname;
+-- if (current_domain !== "arxiv.org") {
+--     let arxiv_pdf_pattern = /http(s)?:\/\/[www.]?arxiv.org\/pdf\//;
+--     let anchors = document.getElementsByTagName('a');
+--     for (let anchor of anchors) {
+--         let href_ = anchor.href;
+--         if (href_.search(arxiv_pdf_pattern) != -1) {
+--             href_ = href_.replace(".pdf", "");
+--             href_ = href_.replace("arxiv.org/pdf/", "arxiv.org/abs/");
+--             anchor.href = href_;
+--         }
+--     }
+-- }
--- test/Main.purs
+++ test/Main.purs
@@ -1,10 +1,31 @@
 module Test.Main where
 
 import Prelude
+import Data.Either (isLeft, isRight)
 import Effect (Effect)
-import Main (euler1)
+import Main (arxivParser)
 import Test.Assert (assert)
+
+import Text.Parsing.Parser (runParser)
 
 main :: Effect Unit
 main = do
-  assert (euler1 == 233168)
+  assert (isLeft $ runParser "" arxivParser)
+  testArxivLink "https" "arxiv.org"
+  testArxivLink "http"  "arxiv.org"
+  testArxivLink "https" "www.arxiv.org"
+  testArxivLink "http"  "www.arxiv.org"
+  assert (isLeft $ runParser "http://arxiv.org//pdf/" arxivParser)
+
+testArxivLink :: String -> String -> Effect Unit
+testArxivLink protocol baseurl = do
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/pdf/1912.00646.pdf"
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/pdf/1912.00646"
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/pdf/1912.00646.pdf?viewer"
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/pdf/1912.00646.pdf?download"
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/abs/1912.00646"
+  assert $ isRight $ test $ protocol <> "://" <> baseurl <> "/abs/1912.00646?extrastuff"
+  where
+    test str = runParser str arxivParser
+
+
